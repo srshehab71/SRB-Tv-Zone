65b330f2d2933e4c9084cbdef7ebdf136f41e974
srshehab71 <shehabsarker9@gmail.com>
2026-07-31 10:00:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const playlistConfig = {
     sources: [
         "https://raw.githubusercontent.com/srshehab71/SRB-Tv-Zone/main/srbtv.m3u",
-        "https://raw.githubusercontent.com/incognitobrothers/AynaOTT-Auto-Update-Playlist/refs/heads/main/ayna_live.m3u",
-        "https://raw.githubusercontent.com/abusaeeidx/Mrgify-BDIX-IPTV/refs/heads/main/playlist.m3u"
+        /*"https://raw.githubusercontent.com/incognitobrothers/AynaOTT-Auto-Update-Playlist/refs/heads/main/ayna_live.m3u",
+        "https://raw.githubusercontent.com/abusaeeidx/Mrgify-BDIX-IPTV/refs/heads/main/playlist.m3u"*/
     ]
 };
